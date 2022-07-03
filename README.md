a362f8e63258604bd13d15e297dfc9fc758c6873
Nikolay Igotti <igotti@gmail.com>
2022-07-03 15:29:30 +0300

    TRacing code

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Useful DTrace scripts
+
+Based on StackOverflow crawling, thanks to all people who posted
+answers.
+
+Run as
+```console
+sudo dtrace -qws tracer.d
+```
---
+++ tracer.d
@@ -0,0 +1,43 @@
+string gTarget;     /* the name of the target executable */
+
+dtrace:::BEGIN
+{
+    gTarget = "simpleui";
+    gTargetPID = -1;
+}
+
+/*
+ * Capture target launch (success)
+ */
+proc:::exec-success
+/ gTarget == execname /
+{
+    gTargetPID = pid;
+    printf("started %s\n", execname);
+    system("sudo dtrace -qws child.d -p %d", pid);
+}
+
+
+syscall::*exit:entry
+/ pid == gTargetPID /
+{
+    gTargetPID = -1;        /* invalidate target pid */
+}
+
+// Demo of how we could track syscalls in children, if needed.
+syscall::open*:entry
+/ ((pid == gTargetPID) || progenyof(gTargetPID)) /
+{
+    self->arg0 = arg0;
+    self->arg1 = arg1;
+}
+
+syscall::open*:return
+/ ((pid == gTargetPID) || progenyof(gTargetPID)) /
+{
+    this->op_kind = ((self->arg1 & O_ACCMODE) == O_RDONLY) ? "READ" : "WRITE";
+    this->path0 = self->arg0 ? copyinstr(self->arg0) : "<nil>";
+
+    //printf("open for %s: <%s> #%d\n", this->op_kind, this->path0, arg0);
+}
+
