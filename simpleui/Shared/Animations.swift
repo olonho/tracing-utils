86742935d4a8dc8f4d444ca6dd7585d3d660edfa
SergeyMalenkov <malenkov@gmail.com>
2022-09-21 15:18:42 +0300

    Animation9 added and ClickMe extracted

--- simpleui/Shared/Animations.swift
+++ simpleui/Shared/Animations.swift
@@ -8,6 +8,18 @@
 import Foundation
 import SwiftUI
 
+private struct ClickMe: View {
+    let text: String
+    let action: () -> Void
+    var body: some View {
+        Button(text, action: action)
+            .padding()
+            .foregroundColor(Color.white)
+            .background(Color.blue)
+            .clipShape(Capsule())
+    }
+}
+
 struct Animation1: View {
     @State var enabled = true
     var body: some View {
@@ -15,14 +27,10 @@ struct Animation1: View {
             Rectangle()
                 .fill(enabled ? Color.green : Color.red)
                 .animation(.easeIn(duration: 3), value: enabled)
-                .frame(width: .infinity, height: 50)
-            Button("Click Me") {
+                .frame(width: nil, height: 50)
+            ClickMe(text: "Click Me") {
                 enabled.toggle()
             }
-            .padding()
-            .foregroundColor(Color.white)
-            .background(Color.blue)
-            .clipShape(Capsule())
             Spacer()
         }
     }
@@ -30,22 +38,16 @@ struct Animation1: View {
 
 struct Animation2: View {
     @State var enabled = true
-    @State var opacity = 1.0
     var body: some View {
         VStack {
             Rectangle()
-                .fill(Color.red.opacity(opacity))
-                .frame(width: .infinity, height: 50)
-            Button("Click Me") {
+                .fill(Color.red)
+                .opacity(enabled ? 1 : 0)
+                .animation(.easeIn(duration: 3), value: enabled)
+                .frame(width: nil, height: 50)
+            ClickMe(text: "Click Me") {
                 enabled.toggle()
-                withAnimation(.easeIn(duration: 3)) {
-                    opacity = enabled ? 1 : 0
-                }
             }
-            .padding()
-            .foregroundColor(Color.white)
-            .background(Color.blue)
-            .clipShape(Capsule())
             Spacer()
         }
     }
@@ -70,9 +72,9 @@ struct Animation3: View {
             .frame(width: 150, height: 150)
             .background(color)
             .clipped()
-            Button("Click Me") {
+            ClickMe(text: "Click Me") {
+                let value = Int.random(in: 0...0xFFFFFF)
                 withAnimation(.easeInOut(duration: 1)) {
-                    let value = Int.random(in: 0...0xFFFFFF)
                     pages.removeAll()
                     pages.append(value)
                     color = Color.init(
@@ -83,11 +85,29 @@ struct Animation3: View {
                         opacity: 1)
                 }
             }
-            .padding()
-            .foregroundColor(Color.white)
-            .background(Color.blue)
-            .clipShape(Capsule())
         }
     }
 }
 
+struct Animation9: View {
+    @State var enabled = true
+    var body: some View {
+        VStack {
+            ZStack {
+                if (enabled) {
+                    Text("Hello, world!")
+                        .transition(.asymmetric(
+                            insertion: .opacity.combined(with: .move(edge: .top)),
+                            removal: .opacity.combined(with: .move(edge: .top))))
+                }
+            }
+            .frame(width: nil, height: 20)
+            ClickMe(text: "Click Me") {
+                withAnimation(.easeOut(duration: 1.5)) {
+                    enabled.toggle()
+                }
+            }
+        }
+        .clipped()
+    }
+}
--- simpleui/Shared/simpleuiApp.swift
+++ simpleui/Shared/simpleuiApp.swift
@@ -15,7 +15,7 @@ struct simpleuiApp: App {
             //DrawingView(current: $current)
             //ContentView()
             //ImageBackgroundView()
-            Animation3()
+            Animation9()
         }
     }
 }
